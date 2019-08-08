ae6ec1cdab2a24bd50a51812dedece522ea0e3d8
Michael Fischer <zarkal@live.com>
2019-08-08 14:40:34 -0700

    Fixed issue preventing circleCi and Heroku from working properly

--- server/server.js
+++ server/server.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
+const { json } = require('body-parser');
 
 const app = express();
+
+app.use(json());
+
+function getWeatherKey() {
+    try {
+        return process.env.WEATHER_API_KEY || require('./weatherKey');
+    }
+    catch (error) {
+        throw new Error(`ERROR: Failed to get an OpenWeather API key from either process.env.WEATHER_API_KEY or ./weatherKey.js in SearchBarActions.js`);
+    }
+}
+
+const WEATHER_KEY = getWeatherKey();
 
 app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
+
+app.post('/api/weather', (req, res) => {
+    const { cityName } = req.body;
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${WEATHER_KEY}`)
+        .then(response => {
+            res.status(200).json(response.data);
+        })
+        .catch((error) => {
+            res.status(404).send(error);
+        });
+})
 
 module.exports = app;
--- src/js/SearchBar/SearchBarActions.js
+++ src/js/SearchBar/SearchBarActions.js
@@ -1,21 +1,9 @@
 import axios from 'axios';
 
-function getWeatherKey() {
-    try {
-        return process.env.WEATHER_API_KEY || require('../../../weatherKey');
-    }
-    catch (error) {
-        console.log(`ERROR: Failed to get an OpenWeather API key from either process.env.WEATHER_API_KEY or ../../../weatherKey.js in SearchBarActions.js`);
-        return ``;
-    }
-}
-
-const weatherKey = getWeatherKey();
-
 export function getWeather(cityName) {
     return {
         type: 'GET_WEATHER', //Should probably limit amount of searches per minute at some point
-        payload: axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${weatherKey}`)
+        payload: axios.post(`/api/weather`, { cityName })
     }
 }
 
